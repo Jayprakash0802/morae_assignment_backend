ef6e038134dfb5b504286c4213327855423c9177
Jayprakash0802 <20bcs063@iiitdwd.ac.in>
2025-08-26 17:38:11 +0530

    changed cors

--- api/index.js
+++ api/index.js
@@ -10,15 +10,26 @@ const routes = require('../routes/routes'); // Adjust path if needed
 
 const app = express();
 
-app.use(cors({
-  origin: ['https://morae-assignment.vercel.app', 
+const allowedOrigins = ['https://morae-assignment.vercel.app', 
     'https://morae-assignment-git-main-jay-prakashs-projects-49a6d6b0.vercel.app/',
     'https://morae-assignment-c8dpd52e7-jay-prakashs-projects-49a6d6b0.vercel.app/',
-  ],
-  credentials: true,
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: 'Content-Type,Authorization'
-}));
+  ];
+
+const corsOptions = {
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      return callback(new Error('Not allowed by CORS'), false);
+    }
+    return callback(null, true);
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(bodyParser.json());
 app.use(express.json());
 
