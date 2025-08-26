9e9e2fb037ff2712c82e3ec8167cbe9fef5a42f5
Jayprakash0802 <20bcs063@iiitdwd.ac.in>
2025-08-26 18:21:08 +0530

    changed cors

--- api/index.js
+++ api/index.js
@@ -10,28 +10,20 @@ const routes = require('../routes/routes'); // Adjust path if needed
 
 const app = express();
 
-const allowedOrigins = ['https://morae-assignment.vercel.app', 
-    'https://morae-assignment-git-main-jay-prakashs-projects-49a6d6b0.vercel.app/',
-    'https://morae-assignment-c8dpd52e7-jay-prakashs-projects-49a6d6b0.vercel.app/',
-  ];
-
-const corsOptions = {
-  origin: function(origin, callback){
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      return callback(new Error('Not allowed by CORS'), false);
-    }
-    return callback(null, true);
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-};
-
-
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 app.use(bodyParser.json());
 app.use(express.json());
+app.use(cors({
+  origin: [
+    'https://morae-assignment.vercel.app',
+    'https://morae-assignment-git-main-jay-prakashs-projects-49a6d6b0.vercel.app',
+    'https://morae-assignment-c8dpd52e7-jay-prakashs-projects-49a6d6b0.vercel.app'
+  ],
+  credentials: true,
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.options('*', cors()); // Always after app.use(cors())
+
 
 const mongoURI = process.env.MONGO_URI;
 
